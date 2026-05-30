e0b04a60c1079941500015142bb8dc0fc1b99286
Max Humphreys <maxjhumphreys@icloud.com>
2026-05-31 00:08:52 +0100

    vulkan inits successfully on mac now

--- engine/src/renderer/vulkan/vulkan_backend.c
+++ engine/src/renderer/vulkan/vulkan_backend.c
@@ -21,12 +21,16 @@ b8 vulkan_backend_init(RendererBackend* renderer_backend, const char* applicatio
   application_info.pEngineName = "Magnetar Engine";
 
   // Set vulkan instance create info
+  const char* const extension_names = {VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME};
+  u32 extension_count = 1;
   VkInstanceCreateInfo create_info = {VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO};
   create_info.pApplicationInfo = &application_info;
-  create_info.enabledExtensionCount = 0;
+  create_info.enabledExtensionCount = extension_count;
   create_info.enabledLayerCount = 0;
-  create_info.ppEnabledExtensionNames = NULL_PTR;
+  create_info.ppEnabledExtensionNames = &extension_names;
   create_info.ppEnabledLayerNames = NULL_PTR;
+  VkInstanceCreateFlags instance_create_flags = VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
+  create_info.flags = instance_create_flags;
 
   VkResult res_create_instance =
       vkCreateInstance(&create_info, vulkan_context.allocator, &vulkan_context.instance);
--- engine/src/renderer/vulkan/vulkan_backend.h
+++ engine/src/renderer/vulkan/vulkan_backend.h
@@ -7,9 +7,22 @@
 #include "define.h"
 #include "renderer/renderer_backend.h"
 
+/**
+ * @brief Creates the vulkan instance and sets up needed layers and extensions. (Called by the
+ * renderer frontend via function pointers in a RendererBackend object.)
+ * @param renderer_backend A pointer to the RendererBackend object.
+ * @param application_name The name to give the VkInstance.
+ * @param platform_state A pointer to the PlatformState object.
+ * @return TRUE if vulkan initialized successfully, otherwise FALSE.
+ */
 b8 vulkan_backend_init(RendererBackend* renderer_backend, const char* application_name,
                        PlatformState* platform_state);
 
+/**
+ * @brief Destroys the VkInstance and other things that need to be banished to the shadow realm.
+ * (Called by the renderer frontend via function pointers in a RendererBackend object.)
+ * @param renderer_backend A pointer to the RendererBackend object.
+ */
 void vulkan_backend_shutdown(RendererBackend* renderer_backend);
 
 b8 vulkan_backend_start_frame(RendererBackend* renderer_backend, f64 delta_time);
